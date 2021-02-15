593f0b8a92ed3baba442ae66925bb1e64c7b76cc
Denis Ershov <W1ns-Smile@yandex.ru>
2021-02-15 16:52:22 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ window.addEventListener("keydown", function (event) {
   }
 });
 
-var player;
+/*var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("video", {
     events: {
@@ -57,9 +57,10 @@ function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING) {
     generateRandomActive();
   }
-}
+}*/
 
 windows.onload = function() {
   let video = prompt('Введите ссылку на YouTube клип');
   document.querySelector('#video').innerHTML = '<iframe width="100%" height="100%" src="'+ video +'?autoplay=1&modestbranding=1&iv_load_policy=3&controls=0&disablekb=1&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>';
+  generateRandomActive();
 }
