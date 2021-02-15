04a10c59b0307e2f554640c37feefbafea1175a2
Denis Ershov <W1ns-Smile@yandex.ru>
2021-02-15 16:40:52 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-//const gamepads = {};
-//const buttonState = {};
 let active = 0;
 let points = 0;
 let streak = 0;
@@ -26,69 +24,29 @@ function buttonPressed(id) {
   document.querySelector("#booster").textContent = `x${booster}`;
 }
 
-//function readValue() {
-  window.addEventListener("keydown", function(event) {
-    if (event.code == "KeyZ") {
-      buttonPressed(2);
-      //window.requestAnimationFrame(readValue);
-    }
-    if (event.code == "KeyX") {
-      buttonPressed(3);
-      //window.requestAnimationFrame(readValue);
-    }
-    if (event.code == "KeyM") {
-      buttonPressed(0);
-      //window.requestAnimationFrame(readValue);
-    }
-    if (event.code == "Comma") {
-      buttonPressed(1);
-      //window.requestAnimationFrame(readValue);
-    }
-  });
-  /*const connectedGamepads = navigator.getGamepads();
-  const gamepadIndexes = Object.keys(gamepads);
-  
-  for (let x = 0; x < gamepadIndexes.length; x++) {
-    const buttons = connectedGamepads[gamepadIndexes[x]].buttons;
-    for (let y = 0; y < buttons.length; y++) {
-      if (buttons[y].pressed) {
-        if (!buttonState[y]) {
-          buttonState[y] = true;
-          buttonPressed(y);
-          console.log('ID кнопки '+y);
-        }
-      } else {
-        delete buttonState[y];
-      }
-    }
-  }*/
-  
-  //if (gamepadIndexes.length > 0) {
-    /*if ("keydown") {
-      window.requestAnimationFrame(readValue);
+window.addEventListener("keydown", function (event) {
+  if (event.code == "KeyZ") {
+    buttonPressed(2);
   }
-}*/
-
-/*window.addEventListener("DOMContentLoaded", function(e) {
-  console.log("Gamepad connected!");
-  gamepads[e.gamepad.index] = true;
-  generateRandomActive();
-  readValue();
-});*/
-
-/*window.addEventListener("gamepaddisconnected", function(e) {
-  console.log("Gamepad disconnected!");
-  delete gamepads[e.gamepad.index];
-});*/
+  if (event.code == "KeyX") {
+    buttonPressed(3);
+  }
+  if (event.code == "KeyM") {
+    buttonPressed(0);
+  }
+  if (event.code == "Comma") {
+    buttonPressed(1);
+  }
+});
 
 var player;
 function onYouTubeIframeAPIReady() {
-    player = new YT.Player('video', {
-      events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
-      }
-    });
+  player = new YT.Player("video", {
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
 }
 
 function onPlayerReady(event) {
